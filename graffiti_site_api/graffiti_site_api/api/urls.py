1a4f4c08c288a52c8d8be8898a683e0f934ea7da
shum-647713 <shum.647713@gmail.com>
2024-01-22 19:29:17 +0300

    add views that return possible routes (api endpoints)

--- graffiti_site_api/graffiti_site_api/api/urls.py
+++ graffiti_site_api/graffiti_site_api/api/urls.py
@@ -3,10 +3,12 @@ from graffiti_site_api.api import views
 
 
 urlpatterns = [
-    path('auth/', include('rest_framework.urls')),
+    path('auth/', include('rest_framework.urls', namespace='auth')),
     path('users/', views.UserListCreate.as_view(), name='user-list'),
     path('users/<username>/', views.UserRetrieve.as_view(), name='user-detail'),
     path('users/<username>/graffiti/', views.UserAddGraffiti.as_view(), name='user-graffiti'),
     path('graffiti/', views.GraffitiListCreate.as_view(), name='graffiti-list'),
     path('graffiti/<pk>/', views.GraffitiRetrieve.as_view(), name='graffiti-detail'),
+    path('', views.api_root),
+    path('auth/', views.api_auth, name='auth'),
 ]
--- graffiti_site_api/graffiti_site_api/api/views.py
+++ graffiti_site_api/graffiti_site_api/api/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from .models import Graffiti
 from .permissions import IsUserThemself
 from rest_framework import generics, permissions
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 class UserListCreate(generics.ListCreateAPIView):
@@ -31,3 +34,18 @@ class UserAddGraffiti(generics.CreateAPIView):
     def perform_create(self, serializer):
         owner = User.objects.get(username = self.kwargs['username'])
         serializer.save(owner = owner)
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'auth': reverse('auth', request=request, format=format),
+        'users': reverse('user-list', request=request, format=format),
+        'graffiti': reverse('graffiti-list', request=request, format=format),
+    })
+
+@api_view(['GET'])
+def api_auth(request, format=None):
+    return Response({
+        'login': reverse('auth:login', request=request, format=format),
+        'logout': reverse('auth:logout', request=request, format=format),
+    })
