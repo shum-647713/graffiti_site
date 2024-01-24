895ca943966a6ceaa021d35f5505a94466c588a3
shum-647713 <shum.647713@gmail.com>
2024-01-24 14:40:20 +0300

    make model 'Photo' change name of uploaded files to sha256 hex digest of file contents; change how objects of models 'Photo' and 'Graffiti' are converted to strings

--- graffiti_site_api/graffiti_site_api/api/models.py
+++ graffiti_site_api/graffiti_site_api/api/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+import os, hashlib
 
 
 class Graffiti(models.Model):
     name = models.CharField(max_length=200)
     owner = models.ForeignKey('auth.User', related_name='graffiti', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+def hash_image_upload(instance, filename):
+    fname, ext = os.path.splitext(filename)
+    ctx = hashlib.sha256()
+    if instance.image.multiple_chunks():
+        for data in instance.image.chunks(4096):
+            ctx.update(data)
+    else:
+        ctx.update(instance.image.read())
+    return os.path.join('images/', ctx.hexdigest() + ext)
 
 class Photo(models.Model):
-    image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to=hash_image_upload, max_length=255)
     graffiti = models.ForeignKey(Graffiti, related_name='photos', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.image.name
