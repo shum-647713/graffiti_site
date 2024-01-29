70b4caba81a7b99e3eff8492dd022b0def05fe2e
shum-647713 <shum.647713@gmail.com>
2024-01-29 13:25:39 +0300

    make api return Location header for created objects and objects location of which may be changed by the action done

--- graffiti_site_api/graffiti_site_api/api/views.py
+++ graffiti_site_api/graffiti_site_api/api/views.py
@@ -2,17 +2,17 @@ from . import serializers
 from django.contrib.auth.models import User
 from .models import Graffiti, Photo
 from .permissions import IsUserThemself, IsGraffitiOwner
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, status
 from rest_framework.decorators import api_view, action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
-from django.http import HttpResponse
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('-pk')
     lookup_field = 'username'
+    
     def get_serializer_class(self):
         match self.action:
             case 'list':
@@ -23,6 +23,7 @@ class UserViewSet(viewsets.ModelViewSet):
                 return serializers.UserUpdateSerializer
             case 'add_graffiti':
                 return serializers.GraffitiSerializer
+    
     def get_permissions(self):
         match self.action:
             case 'list' | 'create' | 'retrieve' | None:
@@ -30,21 +31,32 @@ class UserViewSet(viewsets.ModelViewSet):
             case 'update' | 'partial_update' | 'destroy' | 'add_graffiti':
                 permission_classes = [IsUserThemself]
         return [permission() for permission in permission_classes]
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save()
+        headers = {'Location': reverse('user-detail', request=request, args=[instance.username])}
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
     def update(self, request, *args, **kwargs):
-        username = super().update(request, *args, **kwargs).data['username']
-        response = HttpResponse(content="", status=303)
-        response["Location"] = reverse('user-detail', request=request, args=[username])
-        return response
+        data = super().update(request, *args, **kwargs).data
+        headers = {'Location': reverse('user-detail', request=request, args=[data['username']])}
+        return Response(data, headers=headers)
+    
     @action(detail=True, methods=['post'])
     def add_graffiti(self, request, username=None):
         user = self.get_object()
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(owner = user)
-        return Response()
+        instance = serializer.save(owner = user)
+        headers = {'Location': reverse('graffiti-detail', request=request, args=[instance.pk])}
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class GraffitiViewSet(viewsets.ModelViewSet):
     queryset = Graffiti.objects.all().order_by('-pk')
+    
     def get_serializer_class(self):
         match self.action:
             case 'list':
@@ -53,6 +65,7 @@ class GraffitiViewSet(viewsets.ModelViewSet):
                 return serializers.GraffitiSerializer
             case 'add_photo':
                 return serializers.PhotoSerializer
+    
     def get_permissions(self):
         match self.action:
             case 'list' | 'retrieve' | None:
@@ -62,23 +75,33 @@ class GraffitiViewSet(viewsets.ModelViewSet):
             case 'update' | 'partial_update' | 'destroy' | 'add_photo':
                 permission_classes = [IsGraffitiOwner]
         return [permission() for permission in permission_classes]
-    def perform_create(self, serializer):
-        serializer.save(owner = self.request.user)
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        instance = serializer.save(owner = self.request.user)
+        headers = {'Location': reverse('graffiti-detail', request=request, args=[instance.pk])}
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
     @action(detail=True, methods=['post'])
     def add_photo(self, request, pk=None):
         graffiti = self.get_object()
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save(graffiti = graffiti)
-        return Response()
+        instance = serializer.save(graffiti = graffiti)
+        headers = {'Location': reverse('photo-detail', request=request, args=[instance.pk])}
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
 
 class PhotoList(generics.ListAPIView):
     queryset = Photo.objects.all().order_by('-pk')
     serializer_class = serializers.HyperlinkedPhotoSerializer
 
+
 class PhotoDetail(generics.RetrieveDestroyAPIView):
     queryset = Photo.objects.all()
     serializer_class = serializers.PhotoSerializer
+
 
 @api_view(['GET'])
 def api_root(request, format=None):
@@ -88,6 +111,7 @@ def api_root(request, format=None):
         'graffiti': reverse('graffiti-list', request=request, format=format),
         'photos': reverse('photo-list', request=request, format=format),
     })
+
 
 @api_view(['GET'])
 def auth_list(request, format=None):
