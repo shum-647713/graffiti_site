b5c26b1fbc72ec40d464966b64ca533554a701cc
shum-647713 <shum.647713@gmail.com>
2024-02-25 10:30:32 +0300

    make nginx serve static and media files; make docker compose use volumes, not binds to host dirs

--- graffiti_site/api/urls.py
+++ graffiti_site/api/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path, include
 from . import views
-from django.conf import settings
-from django.conf.urls.static import static
 from rest_framework import routers
 
 
@@ -16,4 +14,4 @@ urlpatterns = [
     path('photos/<pk>/', views.PhotoDetail.as_view(), name='photo-detail'),
     path('auth/', views.auth_list, name='auth-list'),
     path('auth/', include('rest_framework.urls', namespace='auth')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- site.Dockerfile
+++ site.Dockerfile
@@ -5,6 +5,7 @@ WORKDIR /site
 COPY requirements.txt .
 RUN python -m pip install -r requirements.txt
 
+COPY manage.py .
 COPY graffiti_site/ graffiti_site/
 
 CMD ["gunicorn", "-b", "0.0.0.0:8000", "graffiti_site.wsgi"]
